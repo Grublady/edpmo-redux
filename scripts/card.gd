deea1700e4e73ddadd19093a78d8563eef3728a8
Grublady <nucleon.spite_0x@icloud.com>
2025-05-04 14:01:04 -0400

    Improve card layout structure

--- scripts/card.gd
+++ scripts/card.gd
@@ -10,8 +10,8 @@ extends PanelContainer
 			data.changed.connect(_update_data)
 		_update_data()
 
-@onready var title_label: RichTextLabel = $VBoxContainer/Title/RichTextLabel
-@onready var body_label: RichTextLabel = $VBoxContainer/Body/RichTextLabel
+@onready var title_label: RichTextLabel = $Title/MarginContainer/RichTextLabel
+@onready var body_label: RichTextLabel = $Body/MarginContainer/RichTextLabel
 
 func _ready() -> void:
 	_update_data()
