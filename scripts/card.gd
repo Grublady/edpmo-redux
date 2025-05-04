13ba23173e15db4eeb9b71a430674e840454c223
Grublady <nucleon.spite_0x@icloud.com>
2025-05-04 03:18:52 -0400

    Improve quest cards

--- scripts/card.gd
+++ scripts/card.gd
@@ -1,4 +1,21 @@
+@tool
 extends PanelContainer
 
-@export var title: String
-@export_multiline var body: String
+@export var title: String:
+	set(new_value):
+		title = new_value
+		if is_instance_valid(title_label):
+			title_label.text = title
+
+@export_multiline var body: String:
+	set(new_value):
+		body = new_value
+		if is_instance_valid(body_label):
+			body_label.text = body
+
+@onready var title_label: RichTextLabel = $VBoxContainer/Title/RichTextLabel
+@onready var body_label: RichTextLabel = $VBoxContainer/Body/RichTextLabel
+
+func _ready() -> void:
+	title_label.text = title
+	body_label.text = body
