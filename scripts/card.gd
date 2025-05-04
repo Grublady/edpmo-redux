1f33faa62a900bb9d579e434bb7fe40d064fc137
Grublady <nucleon.spite_0x@icloud.com>
2025-05-04 03:04:05 -0400

    Work on quest cards

---
+++ scripts/card.gd
@@ -0,0 +1,4 @@
+extends PanelContainer
+
+@export var title: String
+@export_multiline var body: String
--- scripts/quest_character.gd
+++ scripts/quest_character.gd
@@ -6,13 +6,14 @@ const DANCE_AMOUNT: int = 10
 @export var music_player: AudioStreamPlayer
 
 func _process(_delta: float) -> void:
-	var time := music_player.get_playback_position() + AudioServer.get_time_since_last_mix()
-	var beat := floorf(time / BPM * 60 * 4)
-	begin_bulk_theme_override()
-	if fmod(beat, 2) == 0:
-		add_theme_constant_override(&"margin_top", DANCE_AMOUNT)
-		add_theme_constant_override(&"margin_bottom", 0)
-	else:
-		add_theme_constant_override(&"margin_top", 0)
-		add_theme_constant_override(&"margin_bottom", DANCE_AMOUNT)
-	end_bulk_theme_override()
+	if is_instance_valid(music_player):
+		var time := music_player.get_playback_position() + AudioServer.get_time_since_last_mix()
+		var beat := floorf(time / BPM * 60 * 4)
+		begin_bulk_theme_override()
+		if fmod(beat, 2) == 0:
+			add_theme_constant_override(&"margin_top", DANCE_AMOUNT)
+			add_theme_constant_override(&"margin_bottom", 0)
+		else:
+			add_theme_constant_override(&"margin_top", 0)
+			add_theme_constant_override(&"margin_bottom", DANCE_AMOUNT)
+		end_bulk_theme_override()
